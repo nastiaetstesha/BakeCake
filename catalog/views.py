f196051c21e3fdb5c0d03a5d65447284977a3293
TonyCleem <tonycleem@gmail.com>
2025-08-29 02:27:20 +0700

    исправил base, добавил index

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from models import *
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def cake_customization(request):
+    option_category = OptionCategory.objects.get(slug='levels')
+
+    option_name = Option.objects.filter(
+    return render(request, 'cake_customization.html', context)
