636d063c42b6137a5f5f5294a5f6fa2ec6aff968
Avakova Anastasiia <avakova.ast@gmail.com>
2025-08-28 22:56:00 +0300

    comments

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from models import *
+# from models import *
 
 
 def index(request):
     return render(request, 'index.html')
 
 
-def cake_customization(request):
-    option_category = OptionCategory.objects.get(slug='levels')
+# def cake_customization(request):
+#     option_category = OptionCategory.objects.get(slug='levels')
 
-    option_name = Option.objects.filter(
-    return render(request, 'cake_customization.html', context)
+#     option_name = Option.objects.filter(
+#     return render(request, 'cake_customization.html', context)
